3a400bab0234fabee92f00d69e781fe25916f381
Vitor <vitor.miolo@gestaods.com.br>
2024-08-30 19:55:18 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,100 @@
+<h1 align="center"> Code Race </h1>
+
+# :memo: Descrição
+Projeto para o code race
+
+# :heavy_check_mark: Badges
+![Python](https://img.shields.io/badge/code%20style-black-000000.svg?style=for-the-badge&logoColor=black)
+![Python](https://img.shields.io/badge/python-3670A0?style=for-the-badge&logo=python&logoColor=ffdd54)
+![Django](https://img.shields.io/badge/django-%23092E20.svg?style=for-the-badge&logo=django&logoColor=white)
+![DjangoREST](https://img.shields.io/badge/DJANGO-REST-ff1709?style=for-the-badge&logo=django&logoColor=white&color=ff1709&labelColor=gray)
+![SQLite](https://img.shields.io/badge/sqlite-%2307405e.svg?style=for-the-badge&logo=sqlite&logoColor=white)
+
+## :books: Funcionalidades
+* <b>A definir</b>
+
+## :wrench: Tecnologias utilizadas
+* Django;
+* Python3;
+* Django Rest Framework;
+
+## Dependências
+
+- [Python](https://www.python.org/downloads/) - 3.10+
+- [Django](http://www.djangoproject.com) - django 4.1.5+
+- [BLACK](https://github.com/psf/black)
+
+## :rocket: Instalação:
+
+1. Clone o repositório:
+    ```sh
+    $ git clone https://github.com/Vitor47/loja_virtual.git
+    ```
+2. Instalar dependências e criando ambiente:
+
+    ```sh
+    $ cd projectname
+    $ python3.10 -m venv venv # cria ambiente virtual
+    $ source venv/bin/activate # ativa ambiente virtual linux
+    $ pip install -r requirements.txt
+    ```
+
+3. Criando banco:
+
+    ```sh
+    $ make setup # aplica makemigrations e migrate
+    ```
+    
+4. Criando arquivo .env:
+
+    ```sh
+    $ criar arquivo .env # add na raiz projeto colocar chaves de configuração 
+    ```
+
+5. Executando a aplicação:
+
+    ```sh
+    $ python manage.py runserver # executa o runserver com settings
+    ```
+
+5. Aplicando Linters:
+
+    ```sh
+    $ make lint
+    $ make format
+    ```
+    
+## :on: Indentação
+* Usamos a ferramenta black para manter uma indentação padrão entre os desenvolvedores.
+
+## :soon: Implementação futura
+* On hold
+
+## :handshake: Colaboradores
+
+💜 Um super thanks 👏 para essa galera que fez esse projeto sair :)
+
+<table>
+  <tr>
+    <td align="center">
+      <a href="https://github.com/Vitor47">
+        <img src="https://avatars.githubusercontent.com/u/90582900?v=4" width="100px;" alt="Vitor Miolo"/><br>
+        <sub>
+          <b>Vitor Miolo</b>
+        </sub>
+      </a>
+    </td>
+  </tr>
+</table>
+
+## :dart: Status do projeto
+* On hold 10%
+
+## 💪 Como contribuir para o projeto
+
+1. Faça um **fork** do projeto.
+2. Crie uma nova branch com as suas alterações: `git checkout -b my-feature`
+3. Salve as alterações e crie uma mensagem de commit contando o que você fez: `git commit -m "feature: My new feature"`
+4. Envie as suas alterações: `git push origin my-feature`
+
+## 📝 Licença
---
+++ setup/settings.py
@@ -0,0 +1,134 @@
+import os
+from pathlib import Path
+
+import dj_database_url
+
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = os.getenv("SECRET_KEY")
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = bool(os.getenv("DEBUG", False))
+
+ADMINS = (os.getenv("ADMIN_NAME"), os.getenv("ADMIN_EMAIL"))
+
+MANAGERS = ADMINS
+
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "localhost").split(",")
+
+
+# Application definition
+
+INSTALLED_APPS = [
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "django_bootstrap_icons",
+    "widget_tweaks",
+]
+
+MIDDLEWARE = [
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+]
+
+ROOT_URLCONF = "setup.urls"
+
+TEMPLATES = [
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [os.path.join(BASE_DIR, "templates")],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = "setup.wsgi.application"
+
+
+# Database
+# https://docs.djangoproject.com/en/5.1/ref/settings/#databases
+
+DATABASES = {
+    "default": dj_database_url.config(default=os.environ.get("DATABASE_URL"))
+}
+
+
+# Password validation
+# https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
+    },
+    {
+        "NAME": (
+            "django.contrib.auth.password_validation.MinimumLengthValidator"
+        ),
+    },
+    {
+        "NAME": (
+            "django.contrib.auth.password_validation.CommonPasswordValidator"
+        ),
+    },
+    {
+        "NAME": (
+            "django.contrib.auth.password_validation.NumericPasswordValidator"
+        ),
+    },
+]
+
+
+# Internationalization
+# https://docs.djangoproject.com/en/5.1/topics/i18n/
+
+LANGUAGE_CODE = "pt-BR"
+
+TIME_ZONE = "America/Sao_Paulo"
+
+USE_I18N = True
+
+USE_TZ = True
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/5.1/howto/static-files/
+
+STATIC_URL = "static/"
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
+# STATIC_ROOT = os.path.join('static')
+
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_URL = "/media/"
+
+THOUSAND_SEPARATOR = (".",)
+USE_THOUSAND_SEPARATOR = True
+
+# Default primary key field type
+# https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
