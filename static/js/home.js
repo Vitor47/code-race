849e8f61a9054d0703f2e605f78f4a23c2564ef3
henrico <henrico.an@gmail.com>
2024-08-31 00:24:47 -0300

    feat: get current location

--- static/js/home.js
+++ static/js/home.js
@@ -1,14 +1,34 @@
 function initMap() {
-  console.log(navigator.geolocation)
-  const location = { lat: -34.397, lng: 150.644 };
 
-  const map = new google.maps.Map(document.getElementById("map"), {
-      zoom: 8,
-      center: location,
-  });
-  // The marker, positioned at the location
-  const marker = new google.maps.Marker({
-      position: location,
-      map: map,
-  });
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        const pos = {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude,
+        };
+
+        const map = new google.maps.Map(document.getElementById("map"), {
+          zoom: 8,
+          center: pos,
+        });
+        // The marker, positioned at the location
+        const marker = new google.maps.Marker({
+          position: pos,
+          map: map,
+        });
+      },
+      () => {
+        handleLocationError(true, map.getCenter());
+      }
+    );
+  } else {
+    // Browser doesn't support Geolocation
+    handleLocationError(false, map.getCenter());
+  }
+}
+function handleLocationError(browserHasGeolocation, pos) {
+  alert(browserHasGeolocation
+    ? "Error: The Geolocation service failed."
+    : "Error: Your browser doesn't support geolocation.");
 }
