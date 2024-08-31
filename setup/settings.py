9cfab60986e784a06c68f3c587ba60c103a086fa
Vitor <vitor.miolo@gestaods.com.br>
2024-08-30 23:32:35 -0300

    ajustes

--- setup/settings.py
+++ setup/settings.py
@@ -36,7 +36,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "django_bootstrap_icons",
-    'fontawesomefree',
+    "fontawesomefree",
     "widget_tweaks",
     "apps.map",
 ]
