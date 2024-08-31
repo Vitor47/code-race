e8acf97a83dbefaa17381b1df1659b9ec086d2af
henrico <henrico.an@gmail.com>
2024-08-31 01:22:20 -0300

    feat: search by city

--- static/js/home.js
+++ static/js/home.js
@@ -1,3 +1,5 @@
+let map
+
 function initMap() {
 
   if (navigator.geolocation) {
@@ -8,11 +10,11 @@ function initMap() {
           lng: position.coords.longitude,
         };
 
-        const map = new google.maps.Map(document.getElementById("map"), {
+        map = new google.maps.Map(document.getElementById("map"), {
           zoom: 8,
           center: pos,
         });
-        // The marker, positioned at the location
+
         const marker = new google.maps.Marker({
           position: pos,
           map: map,
@@ -23,10 +25,29 @@ function initMap() {
       }
     );
   } else {
-    // Browser doesn't support Geolocation
     handleLocationError(false, map.getCenter());
   }
 }
+
+function geocodeCity() {
+  const city = document.getElementById("city_input").value;
+  const geocoder = new google.maps.Geocoder();
+
+  geocoder.geocode({ 'address': city }, function (results, status) {
+    if (status === 'OK') {
+      map.setCenter(results[0].geometry.location);
+      map.setZoom(12);
+
+      new google.maps.Marker({
+        position: results[0].geometry.location,
+        map: map,
+      });
+    } else {
+      alert('Geocode was not successful for the following reason: ' + status);
+    }
+  });
+}
+
 function handleLocationError(browserHasGeolocation, pos) {
   alert(browserHasGeolocation
     ? "Error: The Geolocation service failed."
