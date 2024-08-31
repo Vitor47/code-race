f013b45db4c1f59c1b6337b0dae15fbd489c60f2
Lucas <lucasdallmartins@hotmail.com>
2024-08-30 23:55:25 -0300

    input

---
+++ static/js/mock.js
@@ -0,0 +1,13 @@
+const disastres = {
+    enchente: {
+        dica1: "Procure um local elevado, se a água começar a subir, vá para um ponto alto, como um andar superior de um edifício, um telhado ou uma área elevada.",
+        dica2:"Siga as orientações das autoridades, acompanhe as notícias e siga as instruções das autoridades locais, como a Defesa Civil e bombeiros.",
+        dica3:"Não caminhe ou dirija na água, a água pode estar contaminada e a correnteza pode ser perigosa. Além disso, a água pode esconder perigos como buracos e detritos.",
+    },
+    seca:{
+        
+    },
+    queimada:{
+
+    },
+}
